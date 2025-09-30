86e48c0838fa6a514c02e1fc00b598473e6a166c
banaple-swso <swso@banaple.co.kr>
2025-09-30 10:26:54 +0900

    Update Swizzling.swift

--- SkeletonViewCore/Sources/Internal/Helpers/Swizzling.swift
+++ SkeletonViewCore/Sources/Internal/Helpers/Swizzling.swift
@@ -2,14 +2,34 @@
 
 import Foundation
 
+//func swizzle(selector originalSelector: Selector, with swizzledSelector: Selector, inClass: AnyClass, usingClass: AnyClass) {
+//    guard let originalMethod = class_getInstanceMethod(inClass, originalSelector),
+//        let swizzledMethod = class_getInstanceMethod(usingClass, swizzledSelector)
+//        else { return }
+//
+//    if class_addMethod(inClass, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod)) {
+//        class_replaceMethod(inClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+//    } else {
+//        method_exchangeImplementations(originalMethod, swizzledMethod)
+//    }
+//}
+
 func swizzle(selector originalSelector: Selector, with swizzledSelector: Selector, inClass: AnyClass, usingClass: AnyClass) {
     guard let originalMethod = class_getInstanceMethod(inClass, originalSelector),
         let swizzledMethod = class_getInstanceMethod(usingClass, swizzledSelector)
         else { return }
-
+    
     if class_addMethod(inClass, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod)) {
         class_replaceMethod(inClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
-    } else {
-        method_exchangeImplementations(originalMethod, swizzledMethod)
+    }
+    else {
+        guard let originalIMP = class_getMethodImplementation(inClass, originalSelector), let swizzledIMP = class_getMethodImplementation(usingClass, swizzledSelector) else {
+            return
+        }
+        method_setImplementation(originalMethod, swizzledIMP)
+        method_setImplementation(swizzledMethod, originalIMP)
+//        method_exchangeImplementations(originalMethod, swizzledMethod)
+//        
+//        method_setImplementation(origin, <#T##imp: IMP##IMP#>)
     }
 }
